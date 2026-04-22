0727ee34bb3e4467b63e4128777923430a86564d
CarlosGomezIglesias <carlos.gomez.igle@gmail.com>
2026-04-22 16:43:03 +0200

    Eliminado de AndroidManifest.xml el inicio de activitydetail, y añadido en el DetailActivity.kt el binding de ActivityDetailBinding y el inflate

--- app/src/main/java/com/programa1/freetogame/activities/DetailActivity.kt
+++ app/src/main/java/com/programa1/freetogame/activities/DetailActivity.kt
@@ -1,10 +1,28 @@
 package com.programa1.freetogame.activities
 
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.programa1.freetogame.databinding.ActivityDetailBinding
+import com.programa1.freetogame.databinding.ActivityMainBinding
 
 class DetailActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityDetailBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
     }
 }
