8d2b124e40b510655871aba38840d66a4c59bd36
CarlosGomezIglesias <carlos.gomez.igle@gmail.com>
2026-04-22 02:24:50 +0200

    Creando la vista detalle al hacer click en el juego, creado activity_detail, y DetailActivity

---
+++ app/src/main/java/com/programa1/freetogame/activities/DetailActivity.kt
@@ -0,0 +1,10 @@
+package com.programa1.freetogame.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
