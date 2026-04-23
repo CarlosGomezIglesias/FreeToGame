65693a38fdf9c91420bd1804ee4bb927a3caaf67
CarlosGomezIglesias <carlos.gomez.igle@gmail.com>
2026-04-23 19:15:56 +0200

    Mejorado la vision del activity_detail.xmly que se vean los contenidos en la pantalla.

--- app/src/main/java/com/programa1/freetogame/activities/DetailActivity.kt
+++ app/src/main/java/com/programa1/freetogame/activities/DetailActivity.kt
@@ -2,11 +2,13 @@ package com.programa1.freetogame.activities
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.net.toUri
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.programa1.freetogame.R
 import com.programa1.freetogame.data.Game
 import com.programa1.freetogame.data.GameService
 import com.programa1.freetogame.databinding.ActivityDetailBinding
@@ -20,6 +22,7 @@ import retrofit2.http.Query
 class DetailActivity : AppCompatActivity() {
     lateinit var binding: ActivityDetailBinding
     lateinit var game: Game
+    lateinit var developer : GameService
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -48,6 +51,14 @@ class DetailActivity : AppCompatActivity() {
 
     fun loadData() {
         supportActionBar?.title = game.title
+
+        if (game.platform == "Windows") {
+            binding.platform.setImageResource(R.drawable.ic_windows)
+        } else {
+            binding.platform.setImageResource(R.drawable.ic_internet)
+        }
+        binding.developerText.text = game.developer
+        binding.releaseDateText.text = game.releaseDate
 
         Picasso.get().load(game.image).into(binding.thumbnailDetailImageView)
         binding.playButton.setOnClickListener {
